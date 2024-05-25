6ceac3d76dbb04ebcae262992abb2bbc261f366c
FocusFine89 <niivanov02@gmail.com>
2024-05-25 16:47:18 +0200

    change alert Beta function

--- src/Components/AlertBeta.jsx
+++ src/Components/AlertBeta.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import Alert from "react-bootstrap/Alert";
+import { useDispatch, useSelector } from "react-redux";
+import { SET_ALERT } from "../redux/reducers";
 
 function AlertBeta() {
   const [show, setShow] = useState(true);
+  const dispatch = useDispatch();
+  const showAlert = useSelector((state) => state.alert.content);
 
-  if (show) {
+  if (showAlert) {
     return (
-      <Alert variant="info" onClose={() => setShow(false)} dismissible>
+      <Alert
+        variant="info"
+        onClose={() => dispatch({ type: SET_ALERT, payload: false })}
+        dismissible
+      >
         <Alert.Heading>ATTENZIONE</Alert.Heading>
         <p>
           Questa pagina è ancora in <span className="fw-bold">BETA</span>, molte
